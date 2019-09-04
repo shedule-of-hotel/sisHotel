3199b8ddb763f462ff69fac586b687f77017e823
WolfwoodStryfe <54145463+WolfwoodStryfe@users.noreply.github.com>
2019-09-04 09:46:12 -0500

    Primer contacto
    REvisa el VEndor

---
+++ app/Producto.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AguaymantoHotel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    //indicamos referencia a la tabla::::::
+    protected $table = 'producto';
+    //indicamos las conexiones principales de la base de datos
+    protected $primaryKey = 'id_producto';
+
+
+    //para que laravel integre columnas de creaccion  y actualizacion automaticamente
+    public $timestamps=false;
+
+
+    //campos que van a recibir un valor y almacenarlo  en el modelo
+    protected $fillable =[
+        'nombre_producto',
+        'descripcion',
+        'stock',
+        'precio_venta',
+    ];
+
+    // los campos guarder se especifican cuando no deseas que se agreguen al modelo
+    protected $guarded =[
+
+    ];
+
+
+
+    
+}
