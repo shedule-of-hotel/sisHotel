06e0bc8bd2cd028c6bc189861b09b54f5658c8ab
Joseph Pedro Araujo Chuqui <44206652+Joydam-19@users.noreply.github.com>
2019-09-10 05:25:47 -0500

    P

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -5,63 +5,59 @@ namespace AguaymantoHotel\Http\Controllers;
 use Illuminate\Http\Request;
 
 use AguaymantoHotel\Http\Requests; // esto no se creo automaitcamente
-use AguaymantoHotel\Producto;//usamos el nombre de la aplicacion para llamar al modelo
-use Illuminate\Support\facades\redirecf;//hacemos referencia a Redirecf para hacer algunas redirecciones   
-use AguaymantoHotel\Http\Requests\ProductoFormRequest;//hacemos referencia a FormulariodeREquerimientos
-use DB;//usar la base de datos
+use AguaymantoHotel\Producto; //usamos el nombre de la aplicacion para llamar al modelo
+use Illuminate\Support\facades\redirecf; //hacemos referencia a Redirecf para hacer algunas redirecciones   
+use AguaymantoHotel\Http\Requests\ProductoFormRequest; //hacemos referencia a FormulariodeREquerimientos
+use DB; //usar la base de datos
 
 
 class ProductoController extends Controller
 {
     public function __constructor() //creamos el constructor
-    {
-
-    }
+    { }
     public function index(Request $request)
     {
-        if($request)
-        {
-            $query=trim($request->get('searchText'));
-            $productos=DB::table('producto')->where('nombre_producto','LIKE','%'.$query.'%')
-            ->where ('eliminado','=','1')
-            ->orderBy ('id_producto','desc')
-            ->paginate(10);
-            return view('Sistema.Producto.index',["productos"=>$productos,"searchText"=>$query]);
+        if ($request) {
+            $query = trim($request->get('searchText'));
+            $productos = DB::table('producto')->where('nombre_producto', 'LIKE', '%' . $query . '%')
+                ->where('eliminado', '=', '1')
+                ->orderBy('id_producto', 'desc')
+                ->paginate(10);
+            return view('Sistema.Producto.index', ["productos" => $productos, "searchText" => $query]);
         }
     }
-    public function create()//Funcion para Crear en BD
+    public function create() //Funcion para Crear en BD
     {
         return view('Sistema.Producto.create');
     }
-    public function store(ProductoFormRequest $request)//funcion para almacenar en BD
+    public function store(ProductoFormRequest $request) //funcion para almacenar en BD
     {
-        $producto =new Producto;
-        $producto->nombre_producto=$request->get('nombre_producto');
-        $producto->descripcion=$request->get('descripcion');
-        $producto->stock=$request->get('stock');
-        $producto->precio_venta=$request->get('precio_venta');
+        $producto = new Producto;
+        $producto->nombre_producto = $request->get('nombre_producto');
+        $producto->descripcion = $request->get('descripcion');
+        $producto->stock = $request->get('stock');
+        $producto->precio_venta = $request->get('precio_venta');
         $producto->save();
         return Redirect('producto');
     }
-    public function show($id)//funcion para mostrar
+    public function show($id) //funcion para mostrar
     {
-        return view("Sistema.Producto.show",["producto"=>Producto::findOrFail($id)]);
+        return view("Sistema.Producto.show", ["producto" => Producto::findOrFail($id)]);
     }
-    public function edit($id)//funcion para editar
+    public function edit($id) //funcion para editar
     {
-        return view("Sistema.Producto.edit",["producto"=>Producto::findOrFail($id)]);
+        return view("Sistema.Producto.edit", ["producto" => Producto::findOrFail($id)]);
     }
-    public function update(ProductoFormRequest $request,$id)// funcion para actualizar
+    public function update(ProductoFormRequest $request, $id) // funcion para actualizar
     {
-        $producto=Producto::findOrFail($id);
+        $producto = Producto::findOrFail($id);
         $producto->update($request->all());
         return Redirect('producto');
-
     }
-    public function destroy($id)//para eliminar un objeto
+    public function destroy($id) //para eliminar un objeto
     {
-        $producto=Producto::findOrFail($id);
-        $producto->eliminado='0';
+        $producto = Producto::findOrFail($id);
+        $producto->eliminado = '0';
         $producto->update();
         return Redirect('producto');
     }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('producto', 'ProductoController');
-Route::resource('tipohabitacion', 'tipohabController');
+Route::resource('tipohabitacion', 'TipohabController');
