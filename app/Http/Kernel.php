fc69b1458cbe9cba421b52273dd9d24f5185c2ac
WolfwoodStryfe <54145463+WolfwoodStryfe@users.noreply.github.com>
2019-09-16 13:10:41 -0500

    fallando ....

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends HttpKernel
             \AguaymantoHotel\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,//esta linea estaba comentada .... creo que es la que se encarga de evitar que ingrese sin loguearse
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \AguaymantoHotel\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
