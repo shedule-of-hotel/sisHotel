17d173a11a486bbf9a45cd845b589d4acbd99763
Joseph Pedro Araujo Chuqui <44206652+Joydam-19@users.noreply.github.com>
2019-10-03 09:23:38 -0500

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laracasts
                 </div>
 
                 <div class="links">
